cdcad04bb7e60df247273b37b78b6f3eb164a6a0
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2025-01-24 16:52:06 +0200

    add styles errormessage

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,8 +1,23 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, ErrorMessage } from "formik";
 import css from "./RegisterForm.module.css";
 import { useState } from "react";
 import { HiOutlineEye } from "react-icons/hi2";
 import { HiOutlineEyeSlash } from "react-icons/hi2";
+import { emailPattern } from "../../constants";
+import * as Yup from "yup";
+
+const signupSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(emailPattern, "Format example@mail.com")
+    .required("Enter your email"),
+  password: Yup.string()
+    .min(7, "Password should be at least 7 characters!")
+    .required("Enter a valid Password*"),
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 export default function RegisterForm() {
   const [show, setShow] = useState(false);
@@ -13,10 +28,11 @@ export default function RegisterForm() {
   return (
     <div className={css.container}>
       <Formik
-        initialValues={{ name: "", mail: "", password: "" }}
+        initialValues={{ name: "", email: "", password: "" }}
         onSubmit={(values) => {
           console.log("Form submitted with values:", values);
         }}
+        validationSchema={signupSchema}
       >
         <Form className={css.formContainer}>
           <div className={css.inputContainer}>
@@ -31,17 +47,25 @@ export default function RegisterForm() {
               placeholder="Ilona Ratushniak"
             />
           </div>
+          <div>
+            <ErrorMessage name="name" component="span" className={css.error} />
+          </div>
+
           <div className={css.inputContainer}>
-            <label htmlFor="name" className={css.label}>
+            <label htmlFor="email" className={css.label}>
               Mail:
             </label>
             <Field
               type="email"
-              name="mail"
+              name="email"
               className={css.inputMail}
               placeholder="Your@email.com"
             />
           </div>
+          <div>
+            <ErrorMessage name="email" component="span" className={css.error} />
+          </div>
+
           <div className={css.inputContainer}>
             <div className={css.eyeContainer}>
               <label htmlFor="password" className={css.label}>
@@ -60,12 +84,20 @@ export default function RegisterForm() {
               </span>
             </div>
           </div>
-          <button type="submit" className={css.registerButton}>
-            Registration
-          </button>
-          <a href="/login" className={css.link}>
-            Already have an account?
-          </a>
+
+          <ErrorMessage
+            name="password"
+            component="span"
+            className={css.error}
+          />
+          <div className={css.buttonContainer}>
+            <button type="submit" className={css.registerButton}>
+              Registration
+            </button>
+            <a href="/login" className={css.link}>
+              Already have an account?
+            </a>
+          </div>
         </Form>
       </Formik>
     </div>
